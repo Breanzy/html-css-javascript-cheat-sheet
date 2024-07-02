a976f1c52c958e89cc71ce13bb838ad701daabdc
Brean Julius Carbonilla <julius.carbonilla@gmail.com>
2024-07-02 18:43:03 +1000

    Prettier Formatting
    Looks zesty now

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # html-css-cheat-sheet
+
 A test website that demonstrates most of the functions and usage of HTML &amp; CSS elements or properties, along with documentations and tips.
 
 ![ezgif com-gif-maker](https://user-images.githubusercontent.com/76001014/175377038-f574d9fd-7a13-472f-a270-2d38aed21658.gif)
--- index.js
+++ index.js
@@ -13,24 +13,59 @@ line Comment. */
 
 /* 
 
-undefined, null, boolean, string, symbol, number, object
+undefined, null, boolean, string, symbol, number
 
  */
 
-var test1 = "hey";
+var test1 = "hey"; // nobody uses this guy anymore sad. you have to initialize a value for this
 
-let test2 = "hello";
+let test2 = "hello"; // new guy that replaced var. variable can be undefined
 
-const test3 = "sup";
+const test3 = "sup"; // fixed variable. cannot be reassigned
 
+var test4 = {
+    test1: "hey",
+    test2: 3,
+};
 
-var numberTest = (((8*3) / 2) - (1 + 1)) % 3;      // Demonstrates the different operators in js. (* / + - % = Multiply, Divide, Add, Subtract, Modulo)
-numberTest++; numberTest--;                        // Add and Subtract numberTest by 1
-numberTest += 2; numberTest -= 2;                  // Add and Subtract numberTest by 2       NOTE: *= /= is also possible
+test4.test1 = "hello"; // hard code method of accessing object data
+test4[test1] = "world"; // hot code method that allows you to change what to access later on the runtime
 
-console.log(numberTest);                           // Print function for javascript. This print only shows in console at websites though
+test5 = [1, 2, 3]; // array data type
 
-console.log("Adding \"Quotes\" inside ");          // It is tricky to print "" in console.log since code will assume it's end line of string. use \" to bypass
-console.log('Alternative "Quote" ');               // You can use '' instead of "" so you can freely use "" with no extra steps
-console.log(`Even more '"Alternative"' Quote`);    // You can also use `` so you can now use '' freely as well, along with ""
+test5[0] = 6; // accessing array data
 
+var numberTest = ((8 * 3) / 2 - (1 + 1)) % 3; // Demonstrates the different operators in js. (* / + - % = Multiply, Divide, Add, Subtract, Modulo)
+numberTest++;
+numberTest--; // Add and Subtract numberTest by 1
+numberTest += 2;
+numberTest -= 2; // Add and Subtract numberTest by 2       NOTE: *= /= is also possible
+
+console.log(numberTest); // Print function for javascript. This print only shows in console at websites though. You could also use node.js
+
+console.log('Adding "Quotes" inside '); // It is tricky to print "" in console.log since code will assume it's end line of string. use \" to bypass
+console.log('Alternative "Quote" '); // You can use '' instead of "" so you can freely use "" with no extra steps
+console.log(`Even more '"Alternative"' Quote`); // You can also use `` so you can now use '' freely as well, along with ""
+
+function functionTest() {
+    // function format for Javascript
+    return "ayo";
+}
+
+console.log(functionTest()); // template object
+
+function functionTest2() {
+    this.test = "newTest"; // this - it is for referencing the variable of the template object. If you're going to use let instead of this.___,
+} // you will only be adding a value to this function. creating a new object based on this function will not pass newTest value
+
+let test6 = new functionTest2(); // creating a new object functionTest2 using new keyword
+
+console.log(test6.test);
+
+function functionTest() {
+    let b = "test";
+
+    let a = "test";
+
+    return 0;
+}
